3ff5c80dc860901a76d505e7a82d8e6e8477adfa
hamlim <hamlim@rpi.edu>
2013-11-20 13:03:30 -0500

    inserted the PDO Connection code
    This will connect to a database if it exists, need to revise it so it will connect to the db if it exists or creates the db if it does not exist, and then connect to it.

--- install.php
+++ install.php
@@ -1,3 +1,13 @@
 <?php 
 //An installation script to set up the mySQL database 
+  require 'config.php';
+  //config.php is a file that will contain the information needed to connect to the database
+  
+  try {
+    $conn = new PDO('mysql:host='.$config['host'].';dbname='.$config['db_name'],$config['db_username'], $config['db_password']);
+  } catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getmessage();
+  }
+
+
 ?>

82c8172417b33fbace2b51112d49e8567dc33c08
hamlim <hamlim@rpi.edu>
2013-11-21 12:24:28 -0500

    Created a config.php file
    Notice that if you want to run this on a apache server make sure to properly enter your database information!

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  //a simple file that will contain information to connect to a sql server
+  
+  $config = array(
+    'host'        => '',
+    'db_name'     => '',
+    'db_username' => '',
+    'db_password' => '',
+  );
+  

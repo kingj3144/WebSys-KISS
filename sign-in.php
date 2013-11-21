af3f7673ab7da0c890a4410c7c3d018ae355b96b
hamlim <hamlim@rpi.edu>
2013-11-21 13:28:21 -0500

    Added an empty sign-in.php file.
    this will contain both the sign-in sections and the sign-up sections.

---
+++ sign-in.php
@@ -0,0 +1,9 @@
+<?php
+  //Here is the bulk of the sign-in/sign-up page. Here we will render the the user the opportunity to 
+  //  do either action. Note that the idea as of now is to build this modularly so that we can make
+  //  a single page and simply render each php file as needed. This might change in the future.
+  
+  
+  
+  
+?>

eb8c173e0af6502716b46de23969a37ce825fcce
kingj3144 <jjmgruber@gmail.com>
2013-11-23 15:44:19 -0500

    Created a database class with function for connection and initilization

--- config.php
+++ config.php
@@ -2,9 +2,10 @@
   //a simple file that will contain information to connect to a sql server
   
   $config = array(
-    'host'        => '',
-    'db_name'     => '',
-    'db_username' => '',
+    'host'        => 'localhost',
+    'db_name'     => 'Websys7_kiss',
+    'db_username' => 'root',
     'db_password' => '',
+    'db_version'  => '1',
   );
   
--- install.php
+++ install.php
@@ -1,13 +1,41 @@
 <?php 
-//An installation script to set up the mySQL database 
-  require 'config.php';
-  //config.php is a file that will contain the information needed to connect to the database
-  
-  try {
-    $conn = new PDO('mysql:host='.$config['host'].';dbname='.$config['db_name'],$config['db_username'], $config['db_password']);
-  } catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getmessage();
-  }
+	//An installation script to set up the mySQL database 
+	require 'config.php';
+	//config.php is a file that will contain the information needed to connect to the database
+	class KissDatabase
+	{
+		private $conn = NULL;
 
+		/**
+		  * Connects to the mySql server
+		  */
+		public funtion connect() {
+			try {
+	  		$conn = new PDO('mysql:host='.$config['host'],$config['db_username'], $config['db_password']);
+			} catch(PDOException $e) {
+				echo 'ERROR: ' . $e->getmessage();
+			}
+		}
+
+		/**
+		  * Sets up the database and tables if they do not already exisit
+		  */
+		public init() {
+			if ($conn != NULL) {
+				try {
+					//Set up the database
+					$conn->query("CREATE DATABASE IF NOT EXISTS" . $config['db_name'] . $config['db_versioin'] . 
+						"; DEFAULT COLLATE utf8_unicode_ci");
+					$conn->query("USE " . $config['db_name'] . $config['db_versioin']);
+					$conn->exec("CREATE TABLE IF NOT EXISTS users ()");
+					$conn->exec("CREATE TABLE IF NOT EXISTS salts ()");
+					$conn->exec("CREATE TABLE IF NOT EXISTS items ()");
+					
+				} catch(PDOException $e) {
+					echo 'ERROR: ' . $e->getmessage();
+				}
+			}
+		}
+	}
 
 ?>

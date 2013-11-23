0cd796314777328f6a9e926a8399dd5f612c2c9a
kingj3144 <jjmgruber@gmail.com>
2013-11-23 16:36:29 -0500

    Added logic for hashing passwords and creating salts

--- install.php
+++ install.php
@@ -56,18 +56,60 @@
 		  */
 		public getSaltByUser($name) {
 			if ($conn != NULL) {
-				$salt = $conn->query("SELECT * FROM salts WHERE name=$name LIMIT 1");
-				return $salt;
+				try {	
+					$salt = $conn->query("SELECT * FROM salts WHERE name=$name LIMIT 1");
+					return $salt;
+				} catch(PDOException $e) {
+					echo 'ERROR: ' . $e->getmessage();
+				}
 			} else {
 				throw new Exception("Not connected to the database");
 			}
 		}
 
-		public addUser($name, $password) {}
+		/** Adds a new user to the database
+		  * @param $name - the name of the new users as a string
+		  * @param $passworf - the users password as a plaintext string
+		  */
+		public addUser($name, $password) {
+			if ($conn != NULL) {
+				try {	
+					$salt = createSalt();
+					if($conn->exec("INSERT INTO `salts` (`name`, `password`) VALUES ($name, $salt);") == 1) {
+						$hash = hashPassword($password, $salt);
+						//TO DO: user name needs to be escaped of special characters
+						$conn->query("INSERT INTO `users` (`name`, `password`) VALUES ($name, $hash);");
+					} else {
+						throw new Exception("Salt could not be created");
+					}
+				} catch(PDOException $e) {
+					echo 'ERROR: ' . $e->getmessage();
+				}
+			} else {
+				throw new Exception("Not connected to the database");
+			}
+		}
 
-		private hashPassword($password) {}
+		/** Hashes the plaintext passord with the gicen salt
+		  * @param $password - as a plaintext string
+		  * @param - $salt Blowfish salt 
+		  * @return - the hashed password
+		  */
+		private hashPassword($password, $salt) {
+			return crypt($password, $salt);
+		}
 
-		private createSalt() {}
+		/** Creates a random salt compatible with Blowfish hashing
+		  * @return - new random salt
+		  */
+		private createSalt() {
+		    $salt = "";
+		    $salt_chars = array_merge(range('A','Z'), range('a','z'), range(0,9));
+		    for($i=0; $i < 22; $i++) {
+		      $salt .= $salt_chars[array_rand($salt_chars)];
+		    }
+		    return sprintf('$2a$%02d$', $rounds) . $salt;
+		}
 	}
 
 ?>

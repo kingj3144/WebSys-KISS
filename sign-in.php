7cdd937ef627805bafc454f4b5fe9ec779d5ef4e
hamlim <hamlim@rpi.edu>
2013-11-24 12:57:17 -0500

    Started the sign in php page

--- sign-in.php
+++ sign-in.php
@@ -3,7 +3,10 @@
   //  do either action. Note that the idea as of now is to build this modularly so that we can make
   //  a single page and simply render each php file as needed. This might change in the future.
   
-  
+  require 'install.php';
+	
+	
+	
   
   
 ?>

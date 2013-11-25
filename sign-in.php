26d968477c1422e8f8c954f70ba9ec54d13fce6c
hamlim <hamlim@rpi.edu>
2013-11-24 20:14:19 -0500

    Created initial design of signup/in page
    Also added css and js subdirectories with bootstrap core and signin css
    files.

--- sign-in.php
+++ sign-in.php
@@ -7,3 +7,58 @@
   
   
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Kiss Signup/Login page">
+    <meta name="author" content="Tornado Sharks">
+<!--    <link rel="shortcut icon" href="../../assets/ico/favicon.png">-->
+
+    <title>Login or Sign Up for KISS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="resources/css/bootstrap/bootstrap.css" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="resources/css/bootstrap/signin.css" rel="stylesheet" />
+		
+		<!-- Master style sheet for the page -->
+		<link href="resources/css/master.css" rel="stylesheet" />
+  </head>
+
+  <body>
+		<div class="sign-in">
+    	<div class="container">
+
+      <form class="form-signin" method="post">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" class="form-control" placeholder="Username" autofocus/>
+        <input type="password" class="form-control" placeholder="Password"/>
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"/> Remember me
+        </label>
+        <button class="btn btn-lg btn-primary btn-block button" type="submit">Sign in</button>
+      </form>
+
+    	</div> 
+		</div>
+		<div class="sign-up">
+			<div class="container">
+
+      <form class="form-signin" method="post">
+        <h2 class="form-signin-heading">Please create an account:</h2>
+				<input type="text" class="form-control" placeholder="Email Address" name="email"/>
+				<input type="text" class="form-control" placeholder="Name" name="name" />
+				<input type="text" class="form-control" placeholder="Username" name="uname" />
+        <input type="password" class="form-control" placeholder="Password" name="pass" />
+				<input type="password" class="form-control" placeholder="Verify Password" name="verify_pass" />
+        <button class="btn btn-lg btn-primary btn-block button" type="submit">Create Account</button>
+      </form>
+
+    </div>
+		</div>
+
+  </body>
+</html>

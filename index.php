44e482db033c72868ed802a98a3bed5e05044223
Jonah <jjmgruber@gmail.com>
2013-11-30 13:35:34 -0500

    added and index.php which checks login status and redirects accordinly

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	if (session_status() == PHP_SESSION_NONE) {
+		session_start();
+    }
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    	header("location:./lists.php");
+	}else{
+		header("location:./sign-in.php");
+	}
+
+?>

e059e2236104ce51c42979482937050fe6229e1c
Jenn <chanj6@rpi.edu>
2013-12-01 19:01:52 -0500

    Removes items from list.

---
+++ remove.php
@@ -0,0 +1,21 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin'] == true) {
+  header("location:./index.php");
+}
+require_once "config.php";
+require_once "database.php";
+
+if (isset($_GET['itemid'])) {
+	try {
+		$db = new KissDatabase($config);
+		$db->removeItemFromList($_GET['itemid']);
+	} catch(Exception $e) {
+		echo "ERROR: " . $e->getmessage();
+	}
+}
+
+header("location:./lists.php");
+?>
